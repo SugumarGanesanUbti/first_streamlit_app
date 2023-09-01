35aa8785f4dc7b7fc206fb46d67c40d430058936
SugumarGanesanUbti <142973486+SugumarGanesanUbti@users.noreply.github.com>
2023-09-01 15:30:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,9 @@
 import streamlit
 
 streamlit.title('My parents New Healthy Diner')
+
+streamlit.header('🥣 Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥗 🥑🍞Hard-Boiled Free-Range Egg')
+
